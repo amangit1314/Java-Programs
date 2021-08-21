0edf473953736239d82b8bf502d2eb4a2de4f7c8
Aman <gitaman8481@gmail.com>
2021-08-21 10:58:19 +0530

    #Modifiation 58

--- ssp/SSP_Problem_05.java
+++ ssp/SSP_Problem_05.java
@@ -5,37 +5,65 @@ import java.util.Scanner;
 // Problem Title => Maximum and minimum of an array using minimum number of comparisons
 public class SSP_Problem_05 {
 
-    static int floorSqrt(int x){
-        // base case
-        if(x == 0 || x == 1)
-            return x;
-
-        // Do Binary Search for floor(sqrt(x))
-        long start = 1, end = x, ans = 0;
-        while(start <= end){
-            long mid = (start + end) / 2;
-            // checking if that is the square root of middle number
-            if(mid*mid == x)
-                return (int)mid;
-
-            // checking if that is smaller than the square root of middle number
-            if(mid*mid < x){
-                start = mid+1;
-                ans = mid;
-            }
-
-            else
-                end = mid-1;
-        }
-        return (int)ans;
+    static class Pair{
+        int max;
+        int min;
     }
+
+    static Pair getMinMax(int[] arr, int low, int high) {
+        Pair minmax = new Pair();
+        Pair mml = new Pair();
+        Pair mmr = new Pair();
+        int mid;
+
+        // If there is only one element
+        if (low == high) {
+            minmax.max = arr[low];
+            minmax.min = arr[low];
+            return minmax;
+        }
+
+        /* If there are two elements */
+        if (high == low + 1) {
+            if (arr[low] > arr[high]) {
+                minmax.max = arr[low];
+                minmax.min = arr[high];
+            } else {
+                minmax.max = arr[high];
+                minmax.min = arr[low];
+            }
+            return minmax;
+        }
+
+        /* If there are more than 2 elements */
+        mid = (low + high) / 2;
+        mml = getMinMax(arr, low, mid);
+        mmr = getMinMax(arr, mid + 1, high);
+
+        /* compare minimums of two parts*/
+        minmax.min = Math.min(mml.min, mmr.min);
+
+        /* compare maximums of two parts*/
+        minmax.max = Math.max(mml.max, mmr.max);
+
+        return minmax;
+    }
+
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int x = sc.nextInt();
-        System.out.print(floorSqrt(x));
+        int arr_size = sc.nextInt();
+
+        int[] arr = new int[arr_size];
+        for (int i = 0; i < arr_size; i++){
+            arr[i] = sc.nextInt();
+        }
+
+        Pair minmax = getMinMax(arr, 0, arr_size - 1);
+        System.out.printf("\nMinimum element is %d", minmax.min);
+        System.out.printf("\nMaximum element is %d", minmax.max);
     }
 }
 
-// Time Complexity => O(log n) [It means Log n Time will be taken].
+// Time Complexity => O(n) [It means Linear Time will be taken].
 // Space Complexity => O(1) [Constant Time will be taken].
