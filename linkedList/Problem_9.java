ed9dd50bc64d9230af31e4d450d8f2af14a1fcaf
Aman Soni <gitaman8481@gmail.com>
2021-09-07 15:30:45 +0530

    #Modification 75

---
+++ linkedList/Problem_9.java
@@ -0,0 +1,74 @@
+package linkedList;
+// Problem Title => Add “1” to a number represented as a Linked List.
+
+public class Problem_9 {
+
+    static class Node {
+        int data;
+        Node next;
+    }
+
+    static Node newNode(int data) {
+        Node newNode = new Node();
+        newNode.data = data;
+        newNode.next = null;
+        return newNode;
+    }
+
+    // Recursively add 1 from end to beginning and returns carry after all nodes are processed.
+    static int addWithCarry(Node head) {
+        // If linked list is empty, then return carry
+        if (head == null)
+            return 1;
+
+        // Add carry returned by next node call
+        int res = head.data + addWithCarry(head.next);
+
+        // Update data and return new carry
+        head.data = (res) % 10;
+        return (res) / 10;
+    }
+
+    // This function mainly uses addWithCarry().
+    static Node addOne(Node head) {
+
+        // Add 1 to linked list from end to beginning
+        int carry = addWithCarry(head);
+
+        // If there is carry after processing all nodes,
+        // then we need to add a new node to linked list
+        if (carry > 0) {
+            Node newNode = newNode(carry);
+            newNode.next = head;
+            return newNode; // New node becomes head now
+        }
+
+        return head;
+    }
+
+    // A utility function to print a linked list
+    static void printList(Node node) {
+        while (node != null) {
+            System.out.print(node.data);
+            node = node.next;
+        }
+        System.out.println();
+    }
+
+    /* Driver code */
+    public static void main(String[] args) {
+        Node head = newNode(1);
+        head.next = newNode(9);
+        head.next.next = newNode(9);
+        head.next.next.next = newNode(9);
+
+        System.out.print("List is");
+        printList(head);
+
+        head = addOne(head);
+        System.out.println();
+        System.out.print("Resultant list is");
+        printList(head);
+    }
+
+}
