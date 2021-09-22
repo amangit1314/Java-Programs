7c00e724e3727f66d11c2118261b788071221893
Aman Soni <gitaman8481@gmail.com>
2021-09-22 12:26:24 +0530

    #Modification 84

--- sortingAlgorithms/MergeSort.java
+++ sortingAlgorithms/MergeSort.java
@@ -1,30 +1,45 @@
 package sortingAlgorithms;
 
+import java.util.*;
+import java.util.concurrent.ThreadLocalRandom;
+
 public class MergeSort {
-    // Merges two subarrays of arr[].
-    // First subarray is arr[l..m]
-    // Second subarray is arr[m+1..r]
-    void merge(int arr[], int l, int m, int r) {
-        // Find sizes of two subarrays to be merged
+
+    int count = 0;
+
+    public static int getRandomValue(int Min, int Max) {
+        // Get and return the random integer within Min and Max
+        return ThreadLocalRandom.current().nextInt(Min, Max + 1);
+    }
+
+    void merge(int[] arr, int l, int m, int r) {
+
+        // Find sizes of two sub arrays to be merged
         int n1 = m - l + 1;
         int n2 = r - m;
 
-        /* Create temp arrays */
-        int L[] = new int[n1];
-        int R[] = new int[n2];
+        // Create temp arrays
+        int[] L = new int[n1];
+        int[] R = new int[n2];
 
-        /*Copy data to temp arrays*/
-        for (int i = 0; i < n1; ++i)
+        // Copy data to temp arrays
+        for (int i = 0; i < n1; ++i){
             L[i] = arr[l + i];
-        for (int j = 0; j < n2; ++j)
+            count++;
+        }
+
+
+        for (int j = 0; j < n2; ++j){
             R[j] = arr[m + 1 + j];
+            count++;
+        }
 
-        /* Merge the temp arrays */
+        // Merge the temp arrays
 
-        // Initial indexes of first and second subarrays
+        // Initial indexes of first and second sub arrays
         int i = 0, j = 0;
 
-        // Initial index of merged subarry array
+        // Initial index of merged sub array
         int k = l;
         while (i < n1 && j < n2) {
             if (L[i] <= R[j]) {
@@ -36,16 +51,17 @@ public class MergeSort {
                 j++;
             }
             k++;
+//            count++;
         }
 
-        /* Copy remaining elements of L[] if any */
+        // Copy remaining elements of L[] (Left array) if any
         while (i < n1) {
             arr[k] = L[i];
             i++;
             k++;
         }
 
-        /* Copy remaining elements of R[] if any */
+        // Copy remaining elements of R[] (Right array) if any
         while (j < n2) {
             arr[k] = R[j];
             j++;
@@ -53,13 +69,12 @@ public class MergeSort {
         }
     }
 
-    // Main function that sorts arr[l..r] using
-    // merge()
-    void sort(int arr[], int l, int r)
-    {
+    // Main function that sorts arr[l..r] (left to right) using merge()
+    void sort(int[] arr, int l, int r) {
+
         if (l < r) {
             // Find the middle point
-            int m =l+ (r-l)/2;
+            int m = l+(r-l)/2;
 
             // Sort first and second halves
             sort(arr, l, m);
@@ -70,9 +85,8 @@ public class MergeSort {
         }
     }
 
-    /* A utility function to print array of size n */
-    static void printArray(int arr[])
-    {
+    // A utility function to print array of size n
+    static void printArray(int[] arr) {
         int n = arr.length;
         for (int i = 0; i < n; ++i)
             System.out.print(arr[i] + " ");
@@ -80,9 +94,19 @@ public class MergeSort {
     }
 
     // Driver code
-    public static void main(String args[])
-    {
-        int arr[] = { 12, 11, 13, 5, 6, 7 };
+    public static void main(String[] args) {
+
+        int Min = 1, Max = 100;
+
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+
+        int[] arr = new int[n];
+        for (int i = 0; i < n; i++) {
+            arr[i] = getRandomValue(Min, Max);
+            System.out.print(arr[i] + " ");
+        }
+        System.out.println();
 
         System.out.println("Given Array");
         printArray(arr);
@@ -92,5 +116,9 @@ public class MergeSort {
 
         System.out.println("\nSorted array");
         printArray(arr);
+        System.out.println();
+
+        System.out.println("Operations");
+        System.out.println(ob.count);
     }
 }
