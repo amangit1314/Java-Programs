a692a22cb1b3361fa94c3f80755688476d62d2ce
gitaman8481 <gitaman8481@gmail.com>
2021-10-17 22:03:35 +0530

    #Modification 98

---
+++ dp/DP_Problem_59.java
@@ -0,0 +1,80 @@
+package dp;
+
+// Problem Title => Find if a string is interleaved of two other strings
+
+/*Algorithm:
+
+Create a DP array (matrix) of size M*N, where m is the size of the first string and n is the size of the second string. Initialize the matrix to false.
+If the sum of sizes of smaller strings is not equal to the size of the larger string then return false and break the array as they can't be the interleaved to form the larger string.
+Run a nested loop the outer loop from 0 to m and the inner loop from 0 to n. Loop counters are i and j.
+If the values of i and j are both zeroes then mark dp[i][j] as true. If the value of i is zero and j is non-zero and the j-1 character of B is equal to j-1 character of C the assign dp[i][j] as dp[i][j-1] and similarly if j is 0 then match i-1 th character of C and A and if it matches then assign dp[i][j] as dp[i-1][j].
+Take three characters x, y, z as (i-1)th character of A and (j-1)th character of B and (i + j – 1)th character of C.
+if x matches with z and y does not match with z then assign dp[i][j] as dp[i-1][j] similarly if x is not equal to z and y is equal to z then assign dp[i][j] as dp[i][j-1]
+if x is equal to y and y is equal to z then assign dp[i][j] as bitwise OR of dp[i][j-1] and dp[i-1][j].
+return value of dp[m][n].*/
+public class DP_Problem_59 {
+
+    static boolean isInterleaved(String A, String B, String C){
+        int M = A.length(), N = B.length();
+        boolean[][] IL = new boolean[M+1][N+1];
+        if((M + N) != C.length())   return false;
+
+        for(int i = 0; i <= M; i++){
+            for(int j = 0; j <= N; j++){
+                if(i == 0 && j == 0)    IL[i][j] = true;
+
+                else if(i == 0){
+                    if(B.charAt(j - 1) == C.charAt(j - 1))
+                        IL[i][j] = IL[i][j-1];
+                }
+
+                else if(j == 0){
+                    if(A.charAt(i - 1) == C.charAt(i - 1))
+                        IL[i][j] = IL[i-1][j];
+                }
+
+                // Current character of C matches with current character of A, but doesn't match with current character if B
+                else {
+                    boolean b2 = A.charAt(i - 1) == C.charAt(i + j - 1);
+                    boolean b = B.charAt(j - 1) != C.charAt(i + j - 1);
+                    if (b2 && b)
+                        IL[i][j] = IL[i - 1][j];
+
+                    // Current character of C matches with current character of B, but doesn't match with current character if A
+                    else {
+                        if (b2 || b) {
+                            if (b2 && B.charAt(j - 1) == C.charAt(i + j - 1))
+                                IL[i][j] = (IL[i - 1][j] ||
+                                        IL[i][j - 1]);
+                        }
+
+                        // Current character of C matches with that of both A and B
+                        else {
+                            IL[i][j] = IL[i][j - 1];
+                        }
+                    }
+                }
+            }
+        }
+        return IL[M][N];
+    }
+
+    // Function to run test cases
+    static void test(String A, String B, String C) {
+        if (isInterleaved(A, B, C))
+            System.out.println(C + " is interleaved of " +
+                    A + " and " + B);
+        else
+            System.out.println(C + " is not interleaved of " +
+                    A + " and " + B);
+    }
+
+    // Driver code
+    public static void main(String[] args) {
+        test("XXY", "XXZ", "XXZXXXY");
+        test("XY", "WZ", "WZXY");
+        test("XY", "X", "XXY");
+        test("YX", "X", "XXY");
+        test("XXY", "XXZ", "XXXXZY");
+    }
+}
---
+++ dp/DP_Problem_60.java
@@ -0,0 +1,43 @@
+package dp;
+// Problem Title => Maximum Length of Pair Chain
+public class DP_Problem_60 {
+    int a, b;
+
+    public DP_Problem_60(int a, int b){
+        this.a = a;
+        this.b = b;
+    }
+
+    // This function assumes that arr[] is sorted in increasing order according the first(or smaller) values in pairs.
+    static int maxChainLength(DP_Problem_60[] arr, int n){
+        int i, j, max = 0;
+        int[] mcl = new int[n];
+
+        // Initialize MCL(maximum chain length) values for all indexes
+        for(i = 0; i < n; i++)  mcl[i] = 1;
+
+        // Compute optimized chain length values in bottom up manner
+        for(i = 1; i < n; i++)
+            for(j = 0; j < i; j++)
+                if(arr[i].a > arr[j].b && mcl[i] < mcl[j] + 1)
+                    mcl[i] = mcl[j] = 1;
+
+        // mcl[i] now stores the maximum chain length ending with pair i. Pick maximum of all MCL values.
+        for(i = 0; i < n; i++)
+            if(max < mcl[i])
+                max = mcl[i];
+
+        return max;
+    }
+
+    // Driver function
+    public static void main(String[] args) {
+        DP_Problem_60[] arr = new DP_Problem_60[]{
+                new DP_Problem_60(5, 24),
+                new DP_Problem_60(15, 25),
+                new DP_Problem_60(27, 40),
+                new DP_Problem_60(50, 60),
+        };
+        System.out.println("Length of maximum size chain is " + maxChainLength(arr, arr.length));
+    }
+}
